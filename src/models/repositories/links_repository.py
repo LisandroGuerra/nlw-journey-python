17144a452f14a794eb726909fb9bcfa8ca1bc987
Lisandro Guerra <lisandro.digital@gmail.com>
2024-07-10 12:40:42 +0100

    feat: implementing links repository

---
+++ src/models/repositories/links_repository.py
@@ -0,0 +1,34 @@
+from sqlite3 import Connection
+
+class LinksRepository:
+    def __init__(self, conn: Connection) -> None:
+        self.__conn = conn
+
+
+    def link_register(self, link_info: dict) -> None:
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            '''
+            INSERT INTO links 
+                (id, trip_id, link)
+            VALUES (?, ?, ?)
+            ''', (
+                link_info['id'],
+                link_info['trip_id'],
+                link_info['link']
+            )
+        )
+        self.__conn.commit()
+        cursor.close()
+
+
+    def find_links_by_trip_id(self, trip_id: str) -> list[tuple]:
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            '''
+            SELECT * FROM links WHERE trip_id = ?
+            ''', (trip_id,)
+        )
+        trip_links = cursor.fetchall()
+        cursor.close()
+        return trip_links
---
+++ src/models/repositories/links_repository_test.py
@@ -0,0 +1,32 @@
+import pytest
+import uuid
+from faker import Faker
+from src.models.settings.db_connection_handler import db_connection_handler
+from .links_repository import LinksRepository
+
+fake = Faker()
+
+db_connection_handler.connect()
+trip_id = str(uuid.uuid4())
+
+
+@pytest.mark.skip(reason='Database interaction test')
+def test_link_register():
+    conn = db_connection_handler.get_connection()
+    links_repository = LinksRepository(conn)
+
+    link_trip_info = {
+        'id': str(uuid.uuid4()),
+        'trip_id': trip_id,
+        'link': fake.url()
+    }
+
+    links_repository.link_register(link_trip_info)
+
+
+@pytest.mark.skip(reason='Database interaction test')
+def test_find_links_by_trip_id():
+    conn = db_connection_handler.get_connection()
+    links_repository = LinksRepository(conn)
+
+    trip_links = links_repository.find_links_by_trip_id(trip_id)
